0712c965b3a0c9620ceaad00990e96e28242478a
Igor <polygor@gmail.com>
2021-12-30 22:13:38 +0200

    Updates

---
+++ static/js/main.9990b1c6.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-abc"]=this["webpackJsonpreact-abc"]||[]).push([[0],{10:function(e,t,c){"use strict";c.r(t);var n=c(1),r=c.n(n),a=c(3),s=c.n(a),i=(c(8),c(0));var o=function(){return Object(i.jsx)("div",{className:"App",children:Object(i.jsx)("h1",{children:"react-template"})})};s.a.render(Object(i.jsx)(r.a.StrictMode,{children:Object(i.jsx)(o,{})}),document.getElementById("root"))},8:function(e,t,c){}},[[10,1,2]]]);
+//# sourceMappingURL=main.9990b1c6.chunk.js.map
