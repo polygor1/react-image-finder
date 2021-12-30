7b7a3e209b95cef829990e8609999d89d2a5bd49
Igor <polygor@gmail.com>
2021-12-30 22:20:22 +0200

    Updates

---
+++ static/js/main.adc31aa7.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-03-image-finder"]=this["webpackJsonpgoit-react-hw-03-image-finder"]||[]).push([[0],{10:function(e,t,c){"use strict";c.r(t);var n=c(1),r=c.n(n),i=c(3),a=c.n(i),s=(c(8),c(0));var o=function(){return Object(s.jsx)("div",{className:"App",children:Object(s.jsx)("h1",{children:"react-template"})})};a.a.render(Object(s.jsx)(r.a.StrictMode,{children:Object(s.jsx)(o,{})}),document.getElementById("root"))},8:function(e,t,c){}},[[10,1,2]]]);
+//# sourceMappingURL=main.adc31aa7.chunk.js.map
