5ea0d66dc8f1dd8dd9c027e9dbc6c40c000f43e9
Igor <polygor@gmail.com>
2022-01-09 07:38:32 +0200

    debug

--- src/components/ErrorSearch/ErrorSearch.js
+++ src/components/ErrorSearch/ErrorSearch.js
@@ -3,12 +3,9 @@ import PropTypes from 'prop-types';
 import styles from './ErrorSearch.module.css';
 
 export default function ErrorSearch({ message }) {
-  const errorImg = './images/error.png';
-
   return (
     <div className={styles.errorWrapper} role="alert">
       <p>{message}</p>
-      <img src={errorImg} alt="empty" />
     </div>
   );
 }
